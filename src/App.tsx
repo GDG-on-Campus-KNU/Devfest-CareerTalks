fc6e8fcb4e3d5ddbc22d7f14c895f92c33a15ddf
KimKyuHoi <k546kh@gmail.com>
2025-05-26 22:51:24 +0900

    feat: session Info 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
+import { css } from '@emotion/react';
 import Header from './components/Header';
+import SessionInfo from './components/SessionInfo';
 
 function App() {
   return (
     <>
       <Header />
+      <main
+        css={css`
+          display: block;
+          padding-top: 54px;
+        `}
+      >
+        <div
+          css={css`
+            z-index: 10;
+            max-width: 1200px;
+            margin: auto;
+            padding: 0 22px;
+            pointer-events: auto;
+          `}
+        >
+          <SessionInfo />
+        </div>
+      </main>
     </>
   );
 }
